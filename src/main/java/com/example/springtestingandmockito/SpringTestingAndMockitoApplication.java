4d3d118a29373375760cac9120cefe63bc409a1a
Ana Edwards <edwards.ana81@gmail.com>
2022-01-14 18:37:02 +0000

    Project Start Up

---
+++ src/main/java/com/example/springtestingandmockito/SpringTestingAndMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtestingandmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestingAndMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestingAndMockitoApplication.class, args);
+    }
+
+}
